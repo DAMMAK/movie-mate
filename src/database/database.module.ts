e401ed316a4afabd9e159e6fa2f35d3bf0d0180d
DAMMAK <adekoyafelix@gmail.com>
2025-03-21 09:10:30 +0100

    removing environment config from docker

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,23 +1,24 @@
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigService } from '../config/config.service';
+import { ConfigModule } from '@nestjs/config';
 
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRootAsync({
-//       imports: [ConfigModule],
-//       inject: [ConfigService],
-//       useFactory: (configService) => ({
-//         type: 'postgres',
-//         host: configService.dbHost,
-//         port: configService.dbPort,
-//         username: configService.dbUsername,
-//         password: configService.dbPassword,
-//         database: configService.dbDatabase,
-//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-//         synchronize: configService.isDevelopment,
-//       }),
-//     }),
-//   ],
-// })
-// export class DatabaseModule {}
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [AppConfigService],
+      useFactory: (configService) => ({
+        type: 'postgres',
+        host: configService.dbHost,
+        port: configService.dbPort,
+        username: configService.dbUsername,
+        password: configService.dbPassword,
+        database: configService.dbDatabase,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: configService.isDevelopment,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
